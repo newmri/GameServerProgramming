0adcd5b7f04e5386f327897b0ba8cf3f05847c28
newmri <newmri@naver.com>
2017-05-28 21:56:15 +0900

    LOGIN, SIGNUP

---
+++ TermProject/ChessGame/ChessGame/DBDefin.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ID_LEN 10  
+#define PWD_LEN 10  
---
+++ TermProject/ChessGame/ChessGame/stDBInfo.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "DBDefin.h"
+#include <Windows.h>
+
+#pragma pack (push, 1)
+struct Point
+{
+	WORD m_wX, m_wY;
+	WORD m_wZone;
+	short m_wXZone, m_wYZone;
+	Point()
+	{
+		m_wX = 65535, m_wY = 0;
+		m_wZone = 1;
+		m_wXZone = 1, m_wYZone = -1;
+	}
+};
+
+struct stDBInfo
+{
+	char ID[ID_LEN];
+	WORD m_Level = 0;
+	WORD m_MAX_HP = 0;
+	WORD m_HP = 0;
+	UINT m_MAX_EXP = 0;
+	UINT m_EXP = 0;
+	WORD m_Damage = 0;
+	Point m_pos;
+
+};
+
+#pragma pack (pop)
